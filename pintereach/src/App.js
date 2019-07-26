8ef8f44a707e5d62ef6391a767e6a8a42920fa50
Michael Williams <manicdesigner@gmail.com>
2019-07-25 20:59:58 -0400

    App.js + CreateBoard component added

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import CreateBoards from './components/CreateBoards/CreateBoards';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <CreateBoards />
     </div>
   );
 }
