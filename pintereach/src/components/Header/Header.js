e6d1885ab13aa5ac7eb16e26158e244ba68d5e40
Michael Williams <manicdesigner@gmail.com>
2019-07-26 11:45:04 -0400

    feat(Header.js) + header component to display the main header to other components just by importing it in.

---
+++ pintereach/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function() {
+  return (
+    <header className="header">
+      <h1 className="header-logo">Pintereach</h1>
+    </header>
+  );
+}
