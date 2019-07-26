71c5e67ff02b87f963172971f7d478afbd9dc15e
Michael Williams <manicdesigner@gmail.com>
2019-07-26 11:57:01 -0400

    feat(App.js) + Header component added for the header section of the application.

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header/Header';
 import CreateBoards from './components/CreateBoards/CreateBoards';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <CreateBoards />
     </div>
   );
