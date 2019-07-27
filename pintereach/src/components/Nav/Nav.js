94f3056437e33c275150c2eaeff8ed20406f63d1
Michael Williams <manicdesigner@gmail.com>
2019-07-27 10:45:57 -0400

    mv(Nav.js) + change import structure + comment out unused variables + add classnames for future use

--- pintereach/src/components/Nav/Nav.js
+++ pintereach/src/components/Nav/Nav.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './Nav.css';
 import { Route, NavLink } from 'react-router-dom';
 
-import Login from '../Login/Login';
-import Register from '../Register/Register';
-import Entry from '../Entry/Entry';
+import Login from '../LoginRegister/Login';
+import Register from '../LoginRegister/Register';
+// import Entry from '../Entry/Entry';
 
 export default function(props) {
-  const { user } = props;
+  // const { user } = props;
   return (
     <section>
       <nav className="nav">
         {/* <NavLink to="/">Home</NavLink> */}
-        <NavLink to="/" />
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/register">Register</NavLink>
+        {/* <NavLink to="/" /> */}
+        <NavLink to="/login" className="font med right">
+          Login
+        </NavLink>
+        <NavLink to="/register" className="font med left">
+          Register
+        </NavLink>
       </nav>
       {/* create Route for '/' HOME into PrivatePath for user after logged in to use only. */}
       {/* <Route
@@ -22,16 +26,16 @@ export default function(props) {
           exact
           render={props => <Home {...props} user={user} />}
         /> */}
-      <Route path="/" exact component={Entry} />
+      {/*<Route path="/" exact component={Entry} /> */}
       <Route
         path="/login"
         exact
-        render={props => <Login {...props} user={user} />}
+        render={props => <Login {...props} /*user={user}*/ />}
       />
       <Route
         path="/register"
         exact
-        render={props => <Register {...props} user={user} />}
+        render={props => <Register {...props} /*user={user}*/ />}
       />
     </section>
   );
