f85d901c465732237185e53d6957fb2ec8c46882
Michael Williams <manicdesigner@gmail.com>
2019-07-27 09:16:29 -0400

    deadcode(Header) - remove unused imports

--- pintereach/src/components/Header/Header.js
+++ pintereach/src/components/Header/Header.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import './Header.css';
-import { Route, NavLink } from 'react-router-dom';
-
-import Login from '../Login/Login';
-import Register from '../Register/Register';
 
 export default class Header extends Component {
   render() {
