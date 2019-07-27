a65bde216a95b87885ee3e4293c81ae8d1f0df52
Michael Williams <manicdesigner@gmail.com>
2019-07-27 14:41:57 -0400

    comment stuff out in NAv to test Register

--- pintereach/src/components/Nav/Nav.js
+++ pintereach/src/components/Nav/Nav.js
@@ -2,36 +2,29 @@ import React from 'react';
 import './Nav.css';
 import { Route, NavLink } from 'react-router-dom';
 
-import Login from '../LoginRegister/Login';
+// import Login from '../LoginRegister/Login';
 import Register from '../LoginRegister/Register';
 // import Entry from '../Entry/Entry';
 
 export default function(props) {
   // const { user } = props;
+
   return (
     <section>
       <nav className="nav">
-        {/* <NavLink to="/">Home</NavLink> */}
-        {/* <NavLink to="/" /> */}
-        <NavLink to="/login" className="font med right">
+        {/* <NavLink to="/login" className="font med right">
           Login
-        </NavLink>
+        </NavLink> */}
         <NavLink to="/register" className="font med left">
           Register
         </NavLink>
       </nav>
-      {/* create Route for '/' HOME into PrivatePath for user after logged in to use only. */}
+
       {/* <Route
-          path="/"
-          exact
-          render={props => <Home {...props} user={user} />}
-        /> */}
-      {/*<Route path="/" exact component={Entry} /> */}
-      <Route
         path="/login"
         exact
-        render={props => <Login {...props} /*user={user}*/ />}
-      />
+        render={props => <Login {...props} loginUser={props.loginUser} />}
+      /> */}
       <Route
         path="/register"
         exact
