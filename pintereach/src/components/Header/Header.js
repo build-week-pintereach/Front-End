8d41c9537a3df3c9e3993008e5b266b85cc40115
Michael Williams <manicdesigner@gmail.com>
2019-07-27 08:30:36 -0400

    feat(Header.js) + import Route, NavLink + import Login, Register components + in render = nav section w/ nav section for NavLinks to home, login, register + Route Home, Login, Register

--- pintereach/src/components/Header/Header.js
+++ pintereach/src/components/Header/Header.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 
-export default function() {
-  return (
-    <header className="header">
-      <h1 className="header-logo">Pintereach</h1>
-    </header>
-  );
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="header-logo">Pintereach</h1>
+        <div className="nav">
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/register">Register</NavLink>
+        </div>
+      </header>
+    );
+  }
 }
