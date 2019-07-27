87e14c621b80ba55ea7c09beeb4f13c5c4717e29
Michael Williams <manicdesigner@gmail.com>
2019-07-27 09:12:29 -0400

    subcomponent(Header) - remove nav and put in own Component

--- pintereach/src/components/Header/Header.js
+++ pintereach/src/components/Header/Header.js
@@ -7,31 +7,9 @@ import Register from '../Register/Register';
 
 export default class Header extends Component {
   render() {
-    const { user } = this.props;
     return (
       <header className="header">
         <h1 className="header-logo">Pintereach</h1>
-        <nav className="nav">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/register">Register</NavLink>
-        </nav>
-        {/* create Route for '/' HOME into PrivatePath for user after logged in to use only. */}
-        <Route
-          path="/"
-          exact
-          render={props => <Home {...props} user={user} />}
-        />
-        <Route
-          path="/login"
-          exact
-          render={props => <Login {...props} user={user} />}
-        />
-        <Route
-          path="/register"
-          exact
-          render={props => <Register {...props} user={user} />}
-        />
       </header>
     );
   }
