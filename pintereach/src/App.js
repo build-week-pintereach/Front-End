33a4628eb22f9927ba0400bd2fcc48ace70abdf1
Michael Williams <manicdesigner@gmail.com>
2019-07-27 09:17:28 -0400

    App.js - remove unused imports

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
-// import CreateBoards from './components/CreateBoards/CreateBoards';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login/Login';
-import { Route } from 'react-router-dom';
-
-import './App.css';
 import Entry from './components/Entry/Entry';
 
-// import Register from './components/Register/Register';
+import './App.css';
 
 class App extends Component {
   constructor() {
