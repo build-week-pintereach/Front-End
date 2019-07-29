273975c03f651985fbd4fadb5460c3ac71205480
Michael Williams <manicdesigner@gmail.com>
2019-07-29 12:33:34 -0400

    comment out unneded register user in App.js

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,40 +1,35 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Entry from './components/Entry/Entry';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: {
-        username: '',
-        password: '',
-        token: ''
-      },
       articles: [],
       filteredArticles: []
     };
   }
 
-  registerUser = user => {
-    axios.get('https://build-week-pintereach.herokuapp.com/api/auth/register');
-  };
+  // registerUser = user => {
+  //   axios.get('https://build-week-pintereach.herokuapp.com/api/auth/register');
+  // };
 
-  loginUser = () => {
-    axios
-      .post('https://build-week-pintereach.herokuapp.com/api/auth/login', {
-        username: 'test1',
-        password: 'hello'
-      })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  // loginUser = () => {
+  //   axios
+  //     .post('https://build-week-pintereach.herokuapp.com/api/auth/login', {
+  //       username: 'test1',
+  //       password: 'hello'
+  //     })
+  //     .then(response => {
+  //       console.log(response);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
 
   render() {
     // const { user } = this.state;
