5d9b7c9e256adcbeed7bcb54095598ba2f142791
Michael Williams <manicdesigner@gmail.com>
2019-07-30 19:42:11 -0400

    add NavLink PrivateRoute

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, NavLink } from 'react-router-dom';
 
 import './App.css';
 import Login from './components/LoginRegister/Login';
@@ -9,16 +9,17 @@ import Register from './components/LoginRegister/Register';
 import PrivateRoute from './components/PrivateRoute';
 
 class App extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     const token = localStorage.getItem('token');
     return (
       <div className="App">
         <Header />
         {token ? <br /> : <Nav />}
+        {token ? (
+          <NavLink to="/protected" component={PrivateRoute}>
+            Create Board
+          </NavLink>
+        ) : null}
 
         <Switch>
           <Route path="/login" component={Login} />
