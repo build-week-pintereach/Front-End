16a891a4e6b9549ca50678a992fbbebe868d4a09
Michael Williams <manicdesigner@gmail.com>
2019-07-29 21:58:27 -0400

    remove excess comments

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import { Route, Switch } from 'react-router-dom';
-// import axios from 'axios';
+
 import './App.css';
 import Login from './components/LoginRegister/Login';
 import Register from './components/LoginRegister/Register';
@@ -11,32 +11,9 @@ import PrivateRoute from './components/PrivateRoute';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      articles: [],
-      filteredArticles: []
-    };
   }
 
-  // registerUser = user => {
-  //   axios.get('https://build-week-pintereach.herokuapp.com/api/auth/register');
-  // };
-
-  // loginUser = () => {
-  //   axios
-  //     .post('https://build-week-pintereach.herokuapp.com/api/auth/login', {
-  //       username: 'test1',
-  //       password: 'hello'
-  //     })
-  //     .then(response => {
-  //       console.log(response);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // };
-
   render() {
-    // const { user } = this.state;
     const token = localStorage.getItem('token');
     return (
       <div className="App">
